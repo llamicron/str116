9c36289420cac2660f7cef8d027f9bba95de1dcd
David Sweeney <sweeney@tamu.edu>
2018-01-23 23:06:30 -0600

    Adds a buttload of crap about the gospel (checksums)

--- str116/__init__.py
+++ str116/__init__.py
@@ -1,12 +1,64 @@
-import random
+import serial
+import time
+import binascii
+import os
+import logging
+
+settings = {
+    'timeout': 0.05,
+    'baudRate': 19200,
+    'port': '/dev/ttyAMA0',
+    'encoding_type': 'utf8',
+    'MA0': '55',
+    'MA1': 'AA',
+    'MAE': '77',
+    'CN': '02',
+}
+
+def get_checksum(bytestring):
+    checksum = sum(bytearray.fromhex(bytestring))
+    checksumstripped = hex(checksum).replace('0x', '')
+    return checksumstripped.zfill(2)
+
+def prepare_bytestring():
+    s = settings
+    bytestring = '0714' + s['CN'] + '0010'
+    checksum = get_checksum(bytestring)
+    return s['MA0'] + s['MA1'] + bytestring + str(checksum) + s['MAE']
+
+
+logging.basicConfig(filename='logs/str116.log', level=logging.INFO)
 
 class Device(object):
     def __init__(self, methods=None):
-        pass
+        self.ser = Device.connect()
+        self.ser.timeout = settings['timeout']
+        self.bytestring = prepare_bytestring()
 
-    def get_relay(self, relay_num):
-        # Get the actual status
-        return random.choice([True, False])
+    @staticmethod
+    def connect():
+        ''' returns a serial device, or raises an IOError '''
+        try:
+            return serial.Serial(settings['port'], settings['baudRate'])
+        except IOError as er:
+            logging.error("Failed to connect to STR116 device")
+            raise
 
-    def set_relay(self, relay_num, state):
-        pass
+    def write(self, data):
+        data = data.encode(settings['encoding_type'])
+        try:
+            self.ser.write(data)
+        except IOError as err:
+            logging.error("Can't write to the STR116 device (%s)" % (err))
+            raise
+        if self.ser.open:
+            time.sleep(0.02)
+            size = self.ser.inWaiting()
+            if size:
+                data = self.ser.read(size)
+                return binascii.hexlify(data.encode(settings['encoding_type']))
+            return None
+
+
+
+
--- tests/test_device.py
+++ tests/test_device.py
@@ -1,10 +1,11 @@
 import unittest
 
-from str116 import Device
+import str116
 
 class TestDevice(unittest.TestCase):
 
     def setUp(self):
+        self.dev = str116.Device()
         self.relay_methods = {
             'hlt': 0,
             'pump': 1
@@ -12,18 +13,19 @@ class TestDevice(unittest.TestCase):
 
     def test_make_one(self):
         # It'll take relay methods
-        assert type(Device(self.relay_methods)) is Device
+        assert type(str116.Device(self.relay_methods)) is str116.Device
         # But doesn't need them
-        assert type(Device()) is Device
+        assert type(str116.Device()) is str116.Device
 
-    def test_get_relay_status(self):
-        dev = Device()
-        status = dev.get_relay(1)
-        assert status is True or status is False
+    def test_write(self):
+        assert self.dev.write('data')
 
-    def test_set_relay(self):
-        dev = Device()
-        dev.set_relay(0, 1)
-        assert dev.get_relay(0)
-        dev.set_relay(0, 0)
-        assert not dev.get_relay(0)
+    def test_checksum(self):
+        bytestring = '0714020010'
+        checksum = '2d'
+        assert str116.get_checksum(bytestring) == checksum
+
+    def test_prepare_bytestring(self):
+        bytestring = '55AA07140200102d77'
+        print(str116.prepare_bytestring())
+        assert str116.prepare_bytestring() == bytestring
